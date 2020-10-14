5d92b8350f3b66ce5c56e53d04119e0e72934582
shishir75 <shishir.srdr16@gmail.com>
2020-10-14 23:53:17 +0600

    Destructuring in Layout component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Layout = (props) => {
-    return <div>{props.children}</div>;
+const Layout = ({ children }) => {
+    return <div>{children}</div>;
 };
 
 export default Layout;
